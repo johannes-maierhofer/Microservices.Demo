52f8ca6a03247636971b2672ae664a49a9f740bd
Johannes Maierhofer <jmai@ciklum.com>
2023-06-06 10:23:50 +0200

    Added DbContextFixture, TODO: a lot

---
+++ src/BuildingBlocks/Testing/DbContextTestFixture.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace BuildingBlocks.Testing
+{
+    public class DbContextTestFixture<TEntryPoint, TDbContext> : IAsyncLifetime
+        where TEntryPoint : class
+        where TDbContext : DbContext
+    {
+        private readonly WebApplicationFactory<TEntryPoint> _factory;
+
+        public DbContextTestFixture()
+        {
+            _factory = new WebApplicationFactory<TEntryPoint>();
+        }
+
+        public async Task InitializeAsync()
+        {
+            using var scope = _factory.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<TDbContext>();
+            await dbContext.Database.EnsureDeletedAsync();
+            await dbContext.Database.EnsureCreatedAsync();
+
+            await SeedDatabase(dbContext);
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual Task SeedDatabase(TDbContext dbContext)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/Customers/tests/Customers.IntegrationTests/AppTestCollection.cs
@@ -0,0 +1,13 @@
+﻿using BuildingBlocks.Testing;
+
+namespace Customers.IntegrationTests
+{
+    [CollectionDefinition("App")]
+    public class AppTestCollection : ICollectionFixture<MsSqlTestFixture>, ICollectionFixture<RabbitMqTestFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+        // see https://xunit.net/docs/shared-context
+    }
+}
